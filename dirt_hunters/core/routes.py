46d91f9a6527fbe305ccaa441f9a506c6d13f508
Venom <hanslettthedev@gmail.com>
2025-09-05 22:57:09 +0100

    fix: added the email module for sending mails

--- dirt_hunters/core/routes.py
+++ dirt_hunters/core/routes.py
@@ -1,9 +1,9 @@
 import os
 
 from flask import Blueprint, jsonify, redirect, render_template, request, url_for
-from flask_mail import Message
 
-from dirt_hunters import db, mail
+from dirt_hunters import db
+from dirt_hunters.mails import send_email_via_graph_api
 from dirt_hunters.models.models import CustomerRequests, Reviews
 
 core = Blueprint("core", __name__)
@@ -50,34 +50,55 @@ def send_email():
             service_type=data["service"],
             message=data["message"],
         )
+
+        # Build email body
+        subject = f"New Cleaning Service Request - {data['service']}"
+        body = f"""
+            New cleaning service request received:
+
+            Full Name: {data['fullName']}
+            Email: {data['email']}
+            Phone: {data.get('phone', 'Not provided')}
+            Service Type: {data['service']}
+            Message:
+            {data['message']}
+
+            Sent from website(www.dirt-hunters.com) contact form.
+        """
+
+        recipients = [
+            {
+                "emailAddress": {
+                    "address": os.getenv("RECIPIENT_EMAIL"),
+                },
+            }
+        ]
+
+        # Send email to mail account
+        send_email_via_graph_api(subject, recipients, body)
+
+        blob = {
+            "subject": "Thank you! Your Message has been receieved",
+            "body": "Thank you for contacting Dirt-Hunters Cleaning Services. We have recieved your request and we will get back to you shortly",
+            "recipients": [
+                {
+                    "emailAddress": {
+                        "address": data["email"],
+                    },
+                }
+            ],
+        }
+        # Send confirmation message that email has been received
+        send_email_via_graph_api(
+            blob.get("subject"), blob.get("recipients"), blob.get("body")
+        )
+
+        customer_request.is_email_sent = True
         db.session.add(customer_request)
         db.session.commit()
 
-        # # Create email message
-        # msg = Message(
-        #     subject=f"New Cleaning Service Request - {data['service']}",
-        #     recipients=[
-        #         os.getenv("RECIPIENT_EMAIL"),
-        #     ],  # Email where you want to receive requests
-        # )
-        #
-        # # Build email body
-        # msg.body = f"""
-        #     New cleaning service request received:
-        #
-        #     Full Name: {data['fullName']}
-        #     Email: {data['email']}
-        #     Phone: {data.get('phone', 'Not provided')}
-        #     Service Type: {data['service']}
-        #     Message:
-        #     {data['message']}
-        #
-        #     Sent from website(www.dirt-hunters.com) contact form.
-        # """
-        #
-        # mail.send(msg)
-        #
         return jsonify({"message": "Form submitted successfully"}), 200
 
     except Exception as e:
+        print(e)
         return jsonify({"error": f"An unexpected error occured"}), 400
